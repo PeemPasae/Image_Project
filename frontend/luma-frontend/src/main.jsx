b984b3cc647f70b9c234eeaaa284e4b472a541f2
KornSudLor <papangkorn.choowong@gmail.com>
2026-09-06 20:06:38 +0700

    feat: add theme feature -v1

--- frontend/luma-frontend/src/main.jsx
+++ frontend/luma-frontend/src/main.jsx
@@ -5,8 +5,12 @@ import './styles/theme.css'
 import './styles/layout.css'
 import './styles/pages.css'
 
+import { ThemeProvider } from './context/ThemeContext'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
